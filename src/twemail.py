141343400836d75dd3f111695b32f77ae98db344
R Y <rebecca@ryates.me.uk>
2014-09-22 01:06:25 +0100

    PyLint tidyup

--- src/twemail.py
+++ src/twemail.py
@@ -1,4 +1,5 @@
 #!usr/bin/python3
+"""Send your Twitter feed to an email address."""
 
 #third party
 from TwitterAPI import TwitterAPI
@@ -15,8 +16,11 @@ from email.mime.multipart import MIMEMultipart
 from secrets import Secrets
 
 class Twemail:
-
+    """Send your Twitter feed to an email address."""
     def run(self):
+        """
+        Collect all tweets since the last run (or the last five tweets if this the first run). Send them to an email address and record the ID of the most recent tweet collected.
+        """
         last_tweet_id = self.get_last_tweet_id(Secrets.record_path)
         tweets = None
         if last_tweet_id:
@@ -31,23 +35,37 @@ class Twemail:
                 self.record_last_tweet_id(tweets, Secrets.record_path)
             
     def get_last_tweet_id(self, record_path):
+        """
+        Read the ID of the most recent tweet from the logfile at the specified     location. Return None if the logfile doesn't exist.
+        """
         last_tweet_id = None
         try:
             with open(record_path, 'r') as record_file:
                 record = json.load(record_file)
                 last_tweet_id = record['last_tweet_id']
-        except:
+        except IOError:
+            #probably the file doesn't exist yet
+            pass
+        except ValueError:
+            #probably the file is empty
             pass
         return last_tweet_id
 
 
     def get_tweets_since(self, last_tweet_id):
+        """
+        Request all your timeline tweets since the given tweet id. Return a list of processed tweet data.
+        """
         twitter = self._get_authenticated_api()
-        raw_tweets = twitter.request('statuses/home_timeline', {'since_id':last_tweet_id})
+        raw_tweets = twitter.request('statuses/home_timeline',
+            {'since_id':last_tweet_id})
         tweets = self._process_raw_tweets(raw_tweets)
         return tweets
 
     def get_last_five_tweets(self):
+        """
+        Request your last five timeline tweets. Return a list of processed tweet data.
+        """
         twitter = self._get_authenticated_api()
         raw_tweets = twitter.request('statuses/home_timeline', {'count':'5'})
         print(raw_tweets.get_iterator().results)
@@ -55,6 +73,9 @@ class Twemail:
         return tweets
 
     def _process_raw_tweets(self, raw_tweets):
+        """
+        Given raw tweets as returned by the Twitter API, process them into a list of dictionaries containing only the useful information (text, author, datetime and id).
+        """
         tweets = []  
         if raw_tweets.status_code == 200:
             for raw_tweet in raw_tweets:
@@ -65,45 +86,68 @@ class Twemail:
         return tweets
 
     def _get_authenticated_api(self):
-        api = TwitterAPI(Secrets.consumer_key, Secrets.consumer_secret, Secrets.access_token_key, Secrets.access_token_secret, "oAuth1")
+        """
+        Get an authenticated Twitter API object to use for making requests.
+        """
+        api = TwitterAPI(   Secrets.consumer_key, 
+                            Secrets.consumer_secret, 
+                            Secrets.access_token_key, 
+                            Secrets.access_token_secret, "oAuth1")
         return api
 
     def _parse_tweet(self, raw_tweet):
+        """
+        Given a raw tweet as returned by the Twitter API, pick out the useful information for display and return it in a dictionary.
+        """
         tweet = {}
         tweet['author'] = raw_tweet['user']['screen_name']
         tweet['text'] = raw_tweet['text']
         tweet['id'] = raw_tweet['id']
       
-        #these fields are all for display, so convert Twitter's UTC times to local time here
+        #these fields are all for display, so convert
+        #Twitter's UTC times to local time here
         localtz = pytz.timezone(Secrets.local_timezone)
-        local_creation_time = parser.parse(raw_tweet['created_at']).astimezone(localtz)
+        creation_time = parser.parse(raw_tweet['created_at'])
+        local_creation_time = creation_time.astimezone(localtz)
         tweet['datetime'] = local_creation_time
 
         return tweet
 
     def format_tweets(self, tweets):
+        """
+        Given a list of processed tweets, generate a string containing an HTML representation of them.
+        """
         tweet_list = sorted(list(tweets), key = lambda tweet: tweet['datetime'])
 
+        #TODO: needs a nicer way to format your tweets
         content = "\n\n".join("<p><b><a href=\"https://twitter.com/{0}\">{0}:</a> </b>{1}<br/><small><a href=\"https://twitter.com/{0}/status/{3}\">{2}</a></small></p>".format(tweet['author'], tweet['text'], tweet['datetime'].strftime("%A %H:%M"), tweet['id']) for tweet in tweet_list)
 
         return content
 
     def send_email(self, email_content, email_address):
-        ascii_content = email_content.encode('ascii', 'xmlcharrefreplace').decode('ascii')
+        """
+        Send an email with the specified content to the specified address, with both plaintext and HTML parts.
+        """
+        clean_content = email_content.encode('ascii', 'xmlcharrefreplace')
+        ascii_content = clean_content.decode('ascii')
         msg = MIMEMultipart('alternative')
         msg['Subject'] = "Recent tweets"
         msg['From'] = "twemail@noreply.twemail.com"
         msg['To'] = email_address
         msg.attach(MIMEText(ascii_content, 'plain'))
         msg.attach(MIMEText(ascii_content, 'html'))
-        s = smtplib.SMTP('localhost')
-        s.sendmail(email_address, [email_address], msg.as_string())
-        s.quit()
+        smtp = smtplib.SMTP('localhost')
+        smtp.sendmail(email_address, [email_address], msg.as_string())
+        smtp.quit()
         return True
 
     def record_last_tweet_id(self, tweets, record_path):
+        """
+        Given a list of processed tweets, record the most recent (highest) tweet ID in the specified file. For later version changes, also store the current version number (1).
+        """
         if tweets and len(tweets) > 0:
-            tweet_list = sorted(list(tweets), key = lambda tweet: tweet['datetime'])
+            tweet_list = sorted(list(tweets), 
+                key = lambda tweet: tweet['datetime'])
             last_tweet = tweet_list[-1]
             record = {'version':1, 'last_tweet_id':last_tweet['id']}
             with open(record_path, 'w') as record_file:
